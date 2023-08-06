3ec32c1ecc4ea6c33867392bfd5ac891111667a2
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-08-06 20:23:56 +0900

    アクティブハッシュの記述

---
+++ app/models/belonging_department.rb
@@ -0,0 +1,10 @@
+class BelongingDepartment < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'A店' },
+    { id: 3, name: 'B店' },
+    { id: 4, name: 'C店' },
+    { id: 5, name: 'D店' },
+    { id: 6, name: 'E店' },
+  ]
+  end
