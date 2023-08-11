dd184310d4953d58c7b954edbc0572d70b9351f3
1hiromu1 <z2.b6.q9.9725@gmail.com>
2023-08-11 13:21:14 +0900

    バリデーションの記述の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :belonging_department
 
+  validates :belonging_department_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :name, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
